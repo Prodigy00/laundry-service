abff0ad6f20ee261d7badda8dea4bfe970cdb0a8
Prodigy00 <simplygiddy@gmail.com>
2019-08-10 03:44:14 +0100

    creating routes and endpoints

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
-const EventEmitter = require("events");
+const express = require("express");
+const Joi = require("joi");
+const app = express();
+app.use(express.json());
 
-const emitter = new EventEmitter();
+const services = [
+  { id: 1, name: "Wash" },
+  { id: 2, name: "clean" },
+  { id: 3, name: "iron" },
+  { id: 4, name: "dry clean" }
+];
 
-//listen for emit event
-emitter.on("logging", eventArg => {
-  console.log("Logger called", eventArg);
+app.get("/", (req, res) => {
+  res.send("Welcome to my Laundry Service!");
 });
 
-const message = "hello";
-//raise logging event
-emitter.emit("logging", { data: message });
+app.get("/api/services", (req, res) => {
+  res.send(services);
+});
+
+app.post("/api/services", (req, res) => {
+  //input validator
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  Joi.validate(req.body, schema);
+
+  const service = {
+    id: services.length + 1,
+    name: req.body.name
+  };
+
+  services.push(service);
+  res.send(service);
+});
+
+app.get("/api/services/:id", (req, res) => {
+  const service = services.find(
+    service => service.id === parseInt(req.params.id)
+  );
+
+  if (!service)
+    res.status(404).send("The service you are looking for was not found");
+  res.send(service);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
